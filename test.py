2ac2509a0095213dd3fa36c689736d44bd0deafa
david-ortega-molina <63212572+david-ortega-molina@users.noreply.github.com>
2021-06-06 18:22:15 -0700

    added sub

--- test.py
+++ test.py
@@ -6,6 +6,9 @@ class TestCase(unittest.TestCase):
 
     def test_add_1(self):
         self.assertEqual(example.add(1, 2), 3)
+    
+    def test_sub_1(self):
+        swlf.assertEqual(example.sub(3, 1), 2)
 
 
 if __name__ == '__main__':
