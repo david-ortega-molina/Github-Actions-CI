99a940735b52aadac0c63f637346d0a74b48ca9c
david-ortega-molina <63212572+david-ortega-molina@users.noreply.github.com>
2021-06-06 21:01:39 -0700

    added divison

--- test.py
+++ test.py
@@ -13,6 +13,9 @@ class TestCase(unittest.TestCase):
     def test_mult_1(self):
         self.assertEqual(example.sult(2, 2), 4)
 
+    def test_div_1(self):
+        self.assertEqual(example.div(6, 2), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
