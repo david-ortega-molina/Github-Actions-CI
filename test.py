973a584156eb48169269138e5b84fa56178bede5
david-ortega-molina <63212572+david-ortega-molina@users.noreply.github.com>
2021-06-06 18:58:07 -0700

    added mult

--- test.py
+++ test.py
@@ -10,6 +10,9 @@ class TestCase(unittest.TestCase):
     def test_sub_1(self):
         self.assertEqual(example.sub(3, 1), 2)
 
+    def test_mult_1(self):
+        self.assertEqual(example.sult(2, 2), 4)
+
 
 if __name__ == '__main__':
     unittest.main()
